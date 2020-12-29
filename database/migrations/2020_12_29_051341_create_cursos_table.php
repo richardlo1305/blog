61bfe209126f0524a58155ff04ccbef27757dcab
RICARDO LINDARTE <ricardo.lindarte@unillanos.edu.co>
2020-12-29 00:25:16 -0500

    SE AGREGO LA MIGRACION PARA CAREERS U SE MODIFICO LA MIGRACION DE CURSOS

--- database/migrations/2020_12_29_051341_create_cursos_table.php
+++ database/migrations/2020_12_29_051341_create_cursos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Cursos extends Migration
+class CreateCursosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class Cursos extends Migration
      */
     public function up()
     {
-        Schema::create('cursos', function (Blueprint $table){
+        Schema::create('cursos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_career')->constrained('careers');
             $table->string('name');
             $table->text('description');
             $table->timestamps();
