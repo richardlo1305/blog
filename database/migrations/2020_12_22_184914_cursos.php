25e4ee3df2d3caae8fff4c1651915959d9f9ea5e
RICARDO LINDARTE <ricardo.lindarte@unillanos.edu.co>
2020-12-28 23:41:31 -0500

    AGREGANDO CAMBIOS PARA CREAR UNA MIGRACION DE CURSOS

--- database/migrations/2020_12_22_184914_cursos.php
+++ database/migrations/2020_12_22_184914_cursos.php
@@ -13,7 +13,12 @@ class Cursos extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('cursos', function (Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class Cursos extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('cursos');
     }
 }
